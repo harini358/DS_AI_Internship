f5aad5b75692f4906f00c456ccc714dc2ab41bc6
Harini <kh815568@gmail.com>
2026-08-10 13:43:37 +0530

    day1

---
+++ day1/pr1.py
@@ -0,0 +1,5 @@
+age=int(input("enter your age:"))
+if age < 18:
+    print("minor they can't vote")
+else:
+    print("they can vote")
---
+++ day1/pr3.py
@@ -0,0 +1,6 @@
+string=input("enter a string:")
+rev=string[::-1]
+if rev==string:
+    print("palindrome")
+else:
+    print("not a palindrome")
